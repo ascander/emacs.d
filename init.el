f5df3a39e6017bf70da827240a1be088cc2f7d02
Ascander Dost <dostinthemachine@gmail.com>
2018-03-06 22:25:46 -0800

    auto-insert init

--- init.el
+++ init.el
@@ -1,4 +1,37 @@
-;; -*- lexical-binding: t -*-
+;;; init.el --- Part of my Emacs setup               -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Ascander Dost
+
+;; Author: Ascander Dost;; -*- lexical-binding: t -*- <ascander@gigan>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is my Emacs configuration, set up for (mostly) Scala
+;; development, and forked from Steve Purcell's excellent setup at:
+;; https://github.com/purcell/emacs.d .
+
+;;; Code:
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq debug-on-error t)
 
 ;;; This file bootstraps the configuration, which is divided into
@@ -167,6 +200,7 @@
 (require 'init-local nil t)
 
 (provide 'init)
+;;; init.el ends here
 
 ;; Local Variables:
 ;; coding: utf-8
