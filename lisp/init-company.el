15373eb5214d9be6ccbbb05261dd17b3cb880d0e
Ascander Dost <adost@salesforce.com>
2018-03-09 16:59:10 -0800

    disable TAB in company; small tweaks to company

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,5 +1,28 @@
-;; WAITING: haskell-mode sets tags-table-list globally, breaks tags-completion-at-point-function
-;; TODO Default sort order should place [a-z] before punctuation
+;;; init-company.el --- Part of my Emacs setup       -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Ascander Dost
+
+;; Author: Ascander Dost <dostinthemachine@gmail.com>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Settings for `company-mode' completions
+
+;;; Code:
 
 (setq tab-always-indent 'complete)
 (add-to-list 'completion-styles 'initials t)
@@ -12,8 +35,13 @@
     (define-key company-active-map (kbd "M-/") 'company-select-next)
     (define-key company-active-map (kbd "C-n") 'company-select-next)
     (define-key company-active-map (kbd "C-p") 'company-select-previous)
+    ;; disable TAB in `company-mode', freeing it for YASnippet
+    (define-key company-active-map [tab] nil)
+    (define-key company-active-map (kbd "TAB") nil)
     (setq-default company-dabbrev-other-buffers 'all
-                  company-tooltip-align-annotations t))
+                  company-tooltip-align-annotations t
+                  company-tooltip-flip-when-above t
+                  company-show-numbers t))
   (global-set-key (kbd "M-C-/") 'company-complete)
   (when (maybe-require-package 'company-quickhelp)
     (add-hook 'after-init-hook 'company-quickhelp-mode))
@@ -42,6 +70,5 @@
     (add-hook 'company-completion-finished-hook 'sanityinc/page-break-lines-maybe-reenable)
     (add-hook 'company-completion-cancelled-hook 'sanityinc/page-break-lines-maybe-reenable)))
 
-
-
 (provide 'init-company)
+;;; init-company.el ends here
