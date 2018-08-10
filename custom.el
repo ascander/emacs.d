c2d0bb1fa0e9a3c8a0ad428a4b7833b0ae383354
Ascander Dost <adost@salesforce.com>
2018-08-09 19:33:25 -0700

    adding custom.el

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (markdown-mode ensime sbt-mode scala-mode company-math company-statistics company-quickhelp company yasnippet-snippets yasnippet deadgrep whitespace-cleanup-mode rainbow-mode highlight-symbol rainbow-delimiters git-timemachine gitattributes-mode gitignore-mode gitconfig-mode magit ivy-rich ivy-xref smex ace-window ibuffer-vc darkroom smartparens free-keys which-key toc-org org-cliplink org-bullets org-plus-contrib counsel-projectile projectile stripe-buffer ignoramus doom-themes base16-theme solarized-theme spaceline powerline dimmer osx-trash exec-path-from-shell restart-emacs paradox validate benchmark-init delight use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(stripe-hl-line ((t (:inherit region)))))
