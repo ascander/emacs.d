77c6c0052d4cf3fd9561cbd1967b1e64c23fa789
Ascander Dost <adost@salesforce.com>
2018-08-13 23:10:54 -0700

    Derpy hooves

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (dumb-jump expand-region deadgrep whitespace-cleanup-mode rainbow-mode rainbow-delimiters free-keys which-key smartparens-config smartparens writeroom-mode counsel-projectile smex counsel ivy-rich ivy-hydra ivy git-timemachine gitattributes-mode gitignore-mode gitconfig-mode magit what-the-commit projectile focus-autosave-mode ace-window ibuffer-vc ignoramus paradox stripe-buffer dimmer solarized-theme osx-trash delight use-package))))
+    (multiple-cursors dumb-jump expand-region deadgrep whitespace-cleanup-mode rainbow-mode rainbow-delimiters free-keys which-key smartparens-config smartparens writeroom-mode counsel-projectile smex counsel ivy-rich ivy-hydra ivy git-timemachine gitattributes-mode gitignore-mode gitconfig-mode magit what-the-commit projectile focus-autosave-mode ace-window ibuffer-vc ignoramus paradox stripe-buffer dimmer solarized-theme osx-trash delight use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
