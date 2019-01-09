e99cd0d44784291bc625b38ffde1ac5afbb55783
Ascander Dost <adost@salesforce.com>
2019-01-08 21:30:25 -0800

    update profile script

--- profile-init.sh
+++ profile-init.sh
@@ -2,4 +2,4 @@
 
 EMACS_CMD=/Applications/Emacs.app/Contents/MacOS/Emacs
 
-$EMACS_CMD -Q -l ./site-lisp/profile-dotemacs.el -f profile-dotemacs
+$EMACS_CMD -Q -l ./lisp/profile-dotemacs.el -f profile-dotemacs
